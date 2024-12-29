71c728bdf7009eb0bf10756d847f74b60792ae33
Phsadev <phsadev@hotmail.com>
2024-12-29 20:56:41 -0300

    Alteração do botão "Login"

--- login.js
+++ login.js
@@ -1,11 +1,37 @@
-document.querySelector('#loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.querySelector('#uname').value;
-    const password = document.querySelector('#psw').value;
-
-    if (gerenciamentoUsuarios.validarUsuario(username, password)) {
-        window.location.href = '../Agendamento/apresentation/apresentation.html';
-    } else {
-        alert('Login falhou, por favor, tente novamente.');
+document.addEventListener('DOMContentLoaded', function() {
+    // Função para inicializar dados de administrador no Local Storage
+    function initializeAdmin() {
+        const adminUser = {
+            username: 'admin',
+            password: 'admin123'
+        };
+        localStorage.setItem('admin', JSON.stringify(adminUser));
     }
+
+    // Verifica se já existe o usuário admin no Local Storage
+    if (!localStorage.getItem('admin')) {
+        initializeAdmin();
+    }
+
+    // Função para fazer login
+    function login(event) {
+        event.preventDefault();
+        
+        const username = document.querySelector('input[name="uname"]').value;
+        const password = document.querySelector('input[name="psw"]').value;
+        
+        const adminUser = JSON.parse(localStorage.getItem('admin'));
+
+        if (username === adminUser.username && password === adminUser.password) {
+            // Sucesso no login
+            window.location.href = "/Agendamento/apresentation/apresentation.html";
+        } else {
+            alert('Nome de usuário ou senha incorretos');
+        }
+    }
+
+    // Adiciona evento de submit ao formulário de login
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', login);
 });
+
